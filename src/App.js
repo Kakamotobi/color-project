f4f3a8d13cc07a160e93f431ec279568fc0a7b1f
Daeram Chung <daeram.chung@gmail.com>
2021-10-12 18:37:59 +0900

    Added palette

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import "./App.css";
+import React, { Component } from "react";
+import Palette from "./Palette.js";
+import seedColors from "./seedColors.js";
 
-function App() {
-	return <div className="App"></div>;
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<Palette {...seedColors[4]} />
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/Palette.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Palette extends Component {
+	render() {
+		return (
+			<div className="Palette">
+        {/* Navbar goes here */}
+				<div className="Palette__colors">
+          {/* bunch of color boxes */}
+        </div>
+        {/* Footer goes here */}
+			</div>
+		);
+	}
+}
+
+export default Palette;
