5a8f8bfc4e9a207a59f056af645ae94d319ed8c2
Daeram Chung <daeram.chung@gmail.com>
2021-10-14 16:59:52 +0900

    Add PaletteList and links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
+import PaletteList from "./PaletteList.js";
 import Palette from "./Palette.js";
 import seedColors from "./seedColors.js";
 import { generatePalette } from "./colorHelpers.js";
