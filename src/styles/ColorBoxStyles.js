e8ec610f4aa5c2c3182c3c8aaba8b275aa1ba5be
Daeram Chung <daeram.chung@gmail.com>
2021-11-01 23:29:19 +0900

    Add ColorBox responsive styles

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -1,4 +1,5 @@
 import chroma from "chroma-js";
+import sizes from "./sizes.js";
 
 const styles = {
 	ColorBox: {
@@ -13,6 +14,18 @@ const styles = {
 		"&:hover button": {
 			opacity: 1,
 			transition: "500ms",
+		},
+		[sizes.down("lg")]: {
+			width: "25%",
+			height: (props) => (props.showingFullPalette ? "20%" : "50%"),
+		},
+		[sizes.down("md")]: {
+			width: "50%",
+			height: (props) => (props.showingFullPalette ? "10%" : "50%"),
+		},
+		[sizes.down("xs")]: {
+			width: "100%",
+			height: (props) => (props.showingFullPalette ? "5%" : "10%"),
 		},
 	},
 	copyText: {
